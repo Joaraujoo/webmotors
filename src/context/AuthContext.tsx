9ba0d4d32c1607961167187930dc7de97155dec9
Joao Araujo <163578593+Joaraujoo@users.noreply.github.com>
2025-06-06 13:35:25 -0300

    context

---
+++ src/context/AuthContext.tsx
@@ -0,0 +1,57 @@
+import { onAuthStateChanged } from "firebase/auth";
+import { type ReactNode, createContext, useState, useEffect  } from "react";
+import { auth } from "../services/firebaseConnection";
+
+interface AuthProviderProps {
+    children: ReactNode;
+}
+
+type AuthContextDada = {
+    signed: boolean;
+    loadingAuth: boolean;
+}
+
+interface UserProps{
+    uid: string;
+    name: string | null;
+    email: string | null;
+}
+
+export const AuthContext = createContext({} as AuthContextDada)
+
+function AuthProvider({ children }: AuthProviderProps){
+
+    const [user, setUser] = useState<UserProps | null>(null)
+    const [loadingAuth, setLoadingAuth] = useState(true)
+
+    useEffect(() => {
+
+        const unsub = onAuthStateChanged(auth, (user) => {
+            if(user){
+                setUser({
+                    uid: user.uid,
+                    name: user?.displayName,
+                    email: user?.email
+                })
+
+                setLoadingAuth(false)
+            }else{
+                setUser(null)
+                setLoadingAuth(false)
+            }
+        })
+
+        return () => {
+            unsub() 
+        }
+
+    }, [])
+
+    return(
+        <AuthContext.Provider value={{signed: !!user, loadingAuth}}>
+            {children}
+        </AuthContext.Provider>
+    )
+}
+
+export default AuthProvider;
