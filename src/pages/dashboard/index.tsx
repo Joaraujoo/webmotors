f5516cb4351926823c9eba2ea2e70ee6a7983a28
Joao Araujo <163578593+Joaraujoo@users.noreply.github.com>
2025-06-17 21:33:25 -0300

    page dashboard

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -4,9 +4,10 @@ import { DashboardHeader } from "../../components/panelHeader";
 export function Dashboard(){
   return(
     <Container>
-      <DashboardHeader>
-        
-      </DashboardHeader>
+      <DashboardHeader/>
+        <div className="w-full flex">
+          <h1 className="m-auto text-zinc-600">Nenhum carro cadastrado!</h1>
+        </div>
     </Container>
   )
 }
